0f75b4f474e9f3096f7db884a0e590815915de91
George Zhang <gzhang@squarespace.com>
2021-02-11 13:53:59 -0500

    [CMS-39189] Add missing eslint config dep, run build

--- UserAccountsNamespaces.js
+++ UserAccountsNamespaces.js
@@ -1,8 +1,12 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var USER_ACCOUNT_API = exports.USER_ACCOUNT_API = 'UserAccountApi';
-var IS_USER_AUTHENTICATED = exports.IS_USER_AUTHENTICATED = 'isUserAuthenticated';
-var OPEN_ACCOUNT_SCREEN = exports.OPEN_ACCOUNT_SCREEN = 'openAccountScreen';
+exports.OPEN_ACCOUNT_SCREEN = exports.IS_USER_AUTHENTICATED = exports.USER_ACCOUNT_API = void 0;
+var USER_ACCOUNT_API = 'UserAccountApi';
+exports.USER_ACCOUNT_API = USER_ACCOUNT_API;
+var IS_USER_AUTHENTICATED = 'isUserAuthenticated';
+exports.IS_USER_AUTHENTICATED = IS_USER_AUTHENTICATED;
+var OPEN_ACCOUNT_SCREEN = 'openAccountScreen';
+exports.OPEN_ACCOUNT_SCREEN = OPEN_ACCOUNT_SCREEN;
